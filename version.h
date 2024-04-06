64f7b3bdf4d37ef29b87d08adea350405e625cbb
D.A.Tiger <drakarax@seznam.cz>
2024-04-06 13:56:05 +0200

    Increment the project version.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "06";
 	static const char MONTH[] = "05";
-	static const char YEAR[] = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "23.05";
+	static const char YEAR[] = "2024";
+	static const char UBUNTU_VERSION_STYLE[] =  "24.05";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -15,15 +15,15 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR    = 0;
-	static const long MINOR    = 2;
-	static const long REVISION = 4;
-	static const long BUILD = 3;
+	static const long MINOR    = 3;
+	static const long REVISION = 0;
+	static const long BUILD = 0;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 0,2,4,3
-	#define RC_FILEVERSION_STRING "0, 2, 4, 3\0"
-	static const char FULLVERSION_STRING [] = "0.2.4.3";
+	#define RC_FILEVERSION 0,3,0,0
+	#define RC_FILEVERSION_STRING "0, 3, 0, 0\0"
+	static const char FULLVERSION_STRING [] = "0.3.0.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
