d83a4fce6a4c010d49a7d5e2be7f9d56cc883869
D.A.Tiger <drakarax@seznam.cz>
2024-04-21 11:41:50 +0200

    Change version

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "07";
+	static const char DATE[] = "21";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2024";
 	static const char UBUNTU_VERSION_STYLE[] =  "24.04";
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR    = 0;
 	static const long MINOR    = 3;
 	static const long REVISION = 1;
-	static const long BUILD = 0;
+	static const long BUILD = 1;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 0,3,1,0
-	#define RC_FILEVERSION_STRING "0, 3, 1, 0\0"
-	static const char FULLVERSION_STRING [] = "0.3.1.0";
+	#define RC_FILEVERSION 0,3,1,1
+	#define RC_FILEVERSION_STRING "0, 3, 1, 1\0"
+	static const char FULLVERSION_STRING [] = "0.3.1.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
