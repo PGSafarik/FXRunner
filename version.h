8f86e2b6179aacd0bf127a410a31bf83bca907d9
D.A.Tiger <drakarax@seznam.cz>
2024-11-21 19:56:23 +0100

    Update version

--- version.h
+++ version.h
@@ -5,9 +5,9 @@ namespace AutoVersion{
 	
 	//Date Version Types
 	static const char DATE[] = "21";
-	static const char MONTH[] = "04";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2024";
-	static const char UBUNTU_VERSION_STYLE[] =  "24.04";
+	static const char UBUNTU_VERSION_STYLE[] =  "24.11";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR    = 0;
 	static const long MINOR    = 3;
-	static const long REVISION = 1;
-	static const long BUILD = 1;
+	static const long REVISION = 2;
+	static const long BUILD = 0;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 0,3,1,1
-	#define RC_FILEVERSION_STRING "0, 3, 1, 1\0"
-	static const char FULLVERSION_STRING [] = "0.3.1.1";
+	#define RC_FILEVERSION 0,3,2,0
+	#define RC_FILEVERSION_STRING "0, 3, 2, 0\0"
+	static const char FULLVERSION_STRING [] = "24.11 0.3.2.0 Alpha";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
