83cba61b628328d72481d60fdfaceb43bb616787
D.A.Tiger <drakarax@seznam.cz>
2025-08-03 20:56:43 +0200

    Rename src/core/Utils.cpp on StringUtils.cpp.

--- src/core/StringUtils.cpp
+++ src/core/StringUtils.cpp
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License      *
 * along with this program.  If not, see <https://www.gnu.org/licenses/>. *
 *************************************************************************/
-#include "core/Utils.h"
+#include "core/StringUtils.h"
 
 /*** String utils *********************************************************************************/
 FX::FXString& operator <<( FX::FXString &dest, const std::string &source ) {
