beed7e96ffd4b24803b2826a035531dae6b5613e
D.A.Tiger <drakarax@seznam.cz>
2025-08-05 21:44:31 +0200

    Remove of redundant include

--- include/core/Storage.h
+++ include/core/Storage.h
@@ -22,7 +22,6 @@
 * Copyright (c) 2025 D.A.Tiger <drakarax@seznam.cz>                      *
 *************************************************************************/
 #include "defs.h"
-#include "History.h"
 
 /* CSV STREAMER */
 class SubstrStream  {
